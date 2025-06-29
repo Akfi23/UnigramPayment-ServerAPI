4d0c144060e7238c6fdd2d37693c70a0ad18bbc5
Andrey <60118284+Akfi23@users.noreply.github.com>
2025-06-29 16:35:48 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,7 +12,20 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 1000;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+
+    next();
+});
+
 app.use(bodyParser.json());
+app.use(bodyParser.text());
 
 app.use('/api', generalRoutes);
 app.use('/api', authRoutes);
