9c36a84f448abae3035a3285e3d5aa5a4c4d771b
Andrey <60118284+Akfi23@users.noreply.github.com>
2025-08-07 21:57:42 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
 //app.use(bodyParser.json());
 //app.use(bodyParser.text());
 app.use(express.json());
+app.use(bodyParser.text());
 
 app.use('/api', generalRoutes);
 app.use('/api', authRoutes);
