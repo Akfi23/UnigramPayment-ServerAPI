17d08de8b7473fa75648b81eb4bbdf0c4fbc366d
Andrey <60118284+Akfi23@users.noreply.github.com>
2025-08-11 17:55:24 +0300

    Update sessionValidation.js

--- src/handlers/sessionValidation.js
+++ src/handlers/sessionValidation.js
@@ -1,99 +1,3 @@
-// const crypto = require('crypto-js');
-// const jwt = require ('jsonwebtoken');
-
-// require('dotenv').config();
-
-// function startClientCheckIn(request, result)
-// {
-//     const received = request.body.trim();
-//     const expected = process.env.CLIENT_SECRET_KEY;
-
-//     if (received === expected) {
-//         try {
-//             const token = jwt.sign(
-//                 { clientId: 'unity-client' },
-//                 process.env.CLIENT_JWT_SIGN,
-//                 { expiresIn: '24h' }
-//             );
-
-//             console.log('JWT Сгенерирован:', token);
-//             return result.status(200).json({ token });
-
-//         } catch (err) {
-//             console.error('Ошибка генерации JWT:', err);
-//             return result.status(500).json({
-//                 error: 'Internal Server Error',
-//                 details: err.message
-//             });
-//         }
-//     }
-
-//     console.warn('Ключ не совпадает');
-//     return result.status(401).json({ error: 'Unauthorized client, access denied.' });
-// }
-
-// function authenticateClient(request, result, next)
-// {
-//     const authHeader = request.headers['authorization'];
-
-//     if (!authHeader)
-//     {
-//         return result.status(403).json(
-//         {
-//             error: 'Authorization token for client not detected, access denied.'
-//         });
-//     }
-
-//     const token = authHeader.split(' ')[1];
-
-//     jwt.verify(token, process.env.CLIENT_JWT_SIGN, (error, decoded) => 
-//     {
-//         if (error)
-//         {
-//             return result.status(403).json({ error: 'Unauthorized client, access denied.'});
-//         }
-
-//         request.cliendId = decoded.clientId;
-
-//         next();
-//     });
-// }
-
-// function authenticateBot(request, result, next)
-// {
-//     const authHeader = request.headers['authorization'];
-
-//     if (!authHeader)
-//     {
-//         return result.status(403).json(
-//         {
-//             error: 'Authorization token for bot not detected, access denied.'
-//         });
-//     }
-
-//     const token = authHeader.split(' ')[1];
-//     const decryptedToken = crypto.AES.decrypt(token,
-//          process.env.BOT_SECRET_KEY).toString(crypto.enc.Utf8);
-
-//     if (decryptedToken !== process.env.BOT_TOKEN)
-//     {
-//         return result.status(403).json(
-//         {
-//             error: 'Unauthorized source request, access denied'
-//         });
-//     }
-
-//     next();
-// }
-
-// module.exports = 
-// {
-//     startClientCheckIn,
-
-//     authenticateBot,
-//     authenticateClient
-// };
-
 const crypto = require('crypto-js');
 const jwt = require ('jsonwebtoken');
 
@@ -101,28 +5,31 @@ require('dotenv').config();
 
 function startClientCheckIn(request, result)
 {
-    const clientSecretKey = request.body;
+    const received = request.body.trim();
+    const expected = process.env.CLIENT_SECRET_KEY;
 
-    if (clientSecretKey === process.env.CLIENT_SECRET_KEY)
-    {
-        const token = jwt.sign(
-        {
-            clientId: 'unity-client'
-        },
-        process.env.CLIENT_JWT_SIGN,
-        {
-            expiresIn: '24h'
-        });
+    if (received === expected) {
+        try {
+            const token = jwt.sign(
+                { clientId: 'unity-client' },
+                process.env.CLIENT_JWT_SIGN,
+                { expiresIn: '24h' }
+            );
 
-        result.status(200).json({ token });
+            console.log('JWT Сгенерирован:', token);
+            return result.status(200).json({ token });
+
+        } catch (err) {
+            console.error('Ошибка генерации JWT:', err);
+            return result.status(500).json({
+                error: 'Internal Server Error',
+                details: err.message
+            });
+        }
     }
-    else
-    {
-        result.status(401).json(
-        {
-            error: 'Unauthorized client, access denied.'
-        });
-    }
+
+    console.warn('Ключ не совпадает');
+    return result.status(401).json({ error: 'Unauthorized client, access denied.' });
 }
 
 function authenticateClient(request, result, next)
@@ -186,3 +93,96 @@ module.exports =
     authenticateBot,
     authenticateClient
 };
+
+// const crypto = require('crypto-js');
+// const jwt = require ('jsonwebtoken');
+
+// require('dotenv').config();
+
+// function startClientCheckIn(request, result)
+// {
+//     const clientSecretKey = request.body;
+
+//     if (clientSecretKey === process.env.CLIENT_SECRET_KEY)
+//     {
+//         const token = jwt.sign(
+//         {
+//             clientId: 'unity-client'
+//         },
+//         process.env.CLIENT_JWT_SIGN,
+//         {
+//             expiresIn: '24h'
+//         });
+
+//         result.status(200).json({ token });
+//     }
+//     else
+//     {
+//         result.status(401).json(
+//         {
+//             error: 'Unauthorized client, access denied.'
+//         });
+//     }
+// }
+
+// function authenticateClient(request, result, next)
+// {
+//     const authHeader = request.headers['authorization'];
+
+//     if (!authHeader)
+//     {
+//         return result.status(403).json(
+//         {
+//             error: 'Authorization token for client not detected, access denied.'
+//         });
+//     }
+
+//     const token = authHeader.split(' ')[1];
+
+//     jwt.verify(token, process.env.CLIENT_JWT_SIGN, (error, decoded) => 
+//     {
+//         if (error)
+//         {
+//             return result.status(403).json({ error: 'Unauthorized client, access denied.'});
+//         }
+
+//         request.cliendId = decoded.clientId;
+
+//         next();
+//     });
+// }
+
+// function authenticateBot(request, result, next)
+// {
+//     const authHeader = request.headers['authorization'];
+
+//     if (!authHeader)
+//     {
+//         return result.status(403).json(
+//         {
+//             error: 'Authorization token for bot not detected, access denied.'
+//         });
+//     }
+
+//     const token = authHeader.split(' ')[1];
+//     const decryptedToken = crypto.AES.decrypt(token,
+//          process.env.BOT_SECRET_KEY).toString(crypto.enc.Utf8);
+
+//     if (decryptedToken !== process.env.BOT_TOKEN)
+//     {
+//         return result.status(403).json(
+//         {
+//             error: 'Unauthorized source request, access denied'
+//         });
+//     }
+
+//     next();
+// }
+
+// module.exports = 
+// {
+//     startClientCheckIn,
+
+//     authenticateBot,
+//     authenticateClient
+// };
